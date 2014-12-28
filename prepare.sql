e3e2bad9255a9c0f82b75445c194ca7ffe539f49
Yury Stankevich <urykhy@gmail.com>
2014-12-28 18:30:54 +0300

    add index

--- prepare.sql
+++ prepare.sql
@@ -1,12 +1,13 @@
 BEGIN TRANSACTION;
-CREATE TABLE books (id INTEGER PRIMARY KEY, ext TEXT, title TEXT, path TEXT);
-CREATE TABLE data (id INTEGER PRIMARY KEY, data TEXT);
-CREATE TABLE desc (id INTEGER PRIMARY KEY, name TEXT);
-INSERT INTO desc VALUES(1,'author');
-INSERT INTO desc VALUES(2,'genre');
-INSERT INTO desc VALUES(3,'sequence');
 CREATE TABLE meta (id INTEGER PRIMARY KEY, book_id NUMERIC, desc_id NUMERIC, data_id NUMERIC,
 				   FOREIGN KEY (book_id) REFERENCES books(id),
 				   FOREIGN KEY (desc_id) REFERENCES desc(id),
 				   FOREIGN KEY (data_id) REFERENCES data(id));
+CREATE TABLE desc (id INTEGER PRIMARY KEY, name TEXT);
+INSERT INTO `desc` VALUES(1,'author');
+INSERT INTO `desc` VALUES(2,'genre');
+INSERT INTO `desc` VALUES(3,'sequence');
+CREATE TABLE data (id INTEGER PRIMARY KEY, data TEXT);
+CREATE TABLE books (id INTEGER PRIMARY KEY, ext TEXT, title TEXT, path TEXT);
+CREATE INDEX `desc_id_index` ON `meta` (`desc_id` ASC);
 COMMIT;
